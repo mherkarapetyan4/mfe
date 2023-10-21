4f76794759b9483f072bc86b096c80dfcd6f42f6
Mher Karapetyan <mherkarapetyan4@gmail.com>
2023-10-21 13:50:10 +0400

    testing empty sessioon token

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from "./components/MarketingApp";
 export default () => {
   return (
     <div>
-      <h1>Hi thereaaaaadd2d!!</h1>
+      <h1>Hi thered!!</h1>
       <hr />
       <MarketingApp />
     </div>
